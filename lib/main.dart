dd328312281b10d5565d3ca14b9fc0f0c78d61de
Farhan Yousaf <58497319+farhanyousaf786@users.noreply.github.com>
2022-06-16 18:53:52 -0400

    Add documentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'package:provider/provider.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'widgets/add_task_screen.dart';
@@ -8,7 +7,6 @@ import 'widgets/app_info_screen.dart';
 import 'widgets/instructions_screen.dart';
 import 'package:todoapp/screens/settings_screen.dart';
 import 'widgets/bottom_bar.dart';
-
 import 'models/bottom_bar/fab_bottom_app_bar.dart';
 import 'models/bottom_bar/fab_with_icons.dart';
 import 'models/bottom_bar/layout.dart';
@@ -16,22 +14,33 @@ import 'models/task_data.dart';
 
 FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
-
+// this is main class, our project starts from here
 Future<void> main() async {
+    
+    
+ // below are some important attributes, these attributes and function 
+ // are responsible for genrating pop up notificationa and Icon of that notification.
   WidgetsFlutterBinding.ensureInitialized();
 
   var initializationSettingsAndroid =
       new AndroidInitializationSettings('@mipmap/ic_launcher');
+    
   var initializationSettingsIOS = IOSInitializationSettings();
+    
   var initializationSettings = InitializationSettings(
       android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    
   await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   WidgetsFlutterBinding.ensureInitialized();
+    
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    
   WidgetsFlutterBinding.ensureInitialized();
+    
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    
   runApp(ChangeNotifierProvider<TaskData>(
 
     create: (_) => TaskData(),
